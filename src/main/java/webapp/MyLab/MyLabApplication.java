45f9357f081a6f127f4be98d76e37c9cd9fba384
amitsahoo <amitsahoo@localhost.localdomain>
2021-09-05 19:27:55 +0530

    First Commit

---
+++ src/main/java/webapp/MyLab/MyLabApplication.java
@@ -0,0 +1,13 @@
+package webapp.MyLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLabApplication {
+        #lokesh 
+	public static void main(String[] args) {
+		SpringApplication.run(MyLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webapp/MyLab/MyLabApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.MyLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
